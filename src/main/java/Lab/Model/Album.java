51450727b41825d4e1b0aa87d004f3ca150e669d
rajithaunnam <rajithaunnam97@gmail.com>
2025-01-16 09:53:02 +0530

    done

--- src/main/java/Lab/Model/Album.java
+++ src/main/java/Lab/Model/Album.java
@@ -8,33 +8,44 @@ import javax.persistence.*;
 import java.util.List;
 
 /**
- * This is an ORM entity for an Album. This entity is incomplete, and you will have to specify the relationship
- * this Entity will have with other entities. Albums ought to have a many-to-one relationship with artists (many albums
- * may be created by one artist) and a one-to-many relationship with songs (one album may have many songs). Review the
- * other provided entities, Artist and Song, to see examples of @OneToMany and @ManyToOne annotations in use.
+ * This is an ORM entity for an Album. This entity is incomplete, and you will
+ * have to specify the relationship
+ * this Entity will have with other entities. Albums ought to have a many-to-one
+ * relationship with artists (many albums
+ * may be created by one artist) and a one-to-many relationship with songs (one
+ * album may have many songs). Review the
+ * other provided entities, Artist and Song, to see examples of @OneToMany
+ * and @ManyToOne annotations in use.
  */
 
 @Entity
-//For the sake of brevity, the following 4 annotations tell Lombok to generate boilerplate code at compile-time.
+// For the sake of brevity, the following 4 annotations tell Lombok to generate
+// boilerplate code at compile-time.
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Album {
-    //The GeneratedValue annotation allows for Spring to automatically generate a unique ID.
+    // The GeneratedValue annotation allows for Spring to automatically generate a
+    // unique ID.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long albumId;
     private String title;
 
     /**
-     * Review the other model classes to see examples of annotations that link entities.
+     * Review the other model classes to see examples of annotations that link
+     * entities.
      */
+    @ManyToOne
+    @JoinColumn(name = "artist_id", nullable = false)
     private Artist artist;
 
     /**
-     * Review the other model classes to see examples of annotations that link entities.
+     * Review the other model classes to see examples of annotations that link
+     * entities.
      */
+    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Song> songs;
 
     public Album(String title) {
@@ -42,7 +53,8 @@ public class Album {
     }
 
     /**
-     * A custom toString() is provided that avoids recursively serializing related entities.
+     * A custom toString() is provided that avoids recursively serializing related
+     * entities.
      */
     @Override
     public String toString() {
@@ -52,26 +64,3 @@ public class Album {
                 '}';
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
